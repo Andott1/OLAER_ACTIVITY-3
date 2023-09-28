6b58d90c1080139007e9a6a42e2218bc681b6f04
Andott1 <145018255+Andott1@users.noreply.github.com>
2023-09-28 18:49:24 +0800

    Add files via upload

---
+++ OLAER_SalesTaxActivity.py
@@ -0,0 +1,16 @@
+# This program calculates the 6% tax amount of user inputted amount.
+
+# Prompts user to input amount
+Amount = float(input("Enter Amount: "))
+
+#Constant value of 6% tax
+TAX = 0.06
+
+#Calculates tax amount
+Tax = Amount * TAX
+
+#Rounds the Tax variable to 2 decimal
+Rounded_num = round(Tax, 2)
+
+#Prints the rounded tax amount
+print("6% Tax Amount: " + str(Rounded_num))
